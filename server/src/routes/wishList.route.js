475f3dd6cce3b5c429245a26ee4ee10ecde1f605
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-03-07 19:10:28 +0530

    authentication check added

--- server/src/routes/wishList.route.js
+++ server/src/routes/wishList.route.js
@@ -4,8 +4,13 @@ const {
   getWishList,
 } = require("../controller/wishList.controller");
 
+const { isAuthenticated } = require("../middleware/auth");
+
 const wishListRouter = express.Router();
 
-wishListRouter.route("/wishlist").post(addToWishList).get(getWishList);
+wishListRouter
+  .route("/wishlist")
+  .post(isAuthenticated, addToWishList)
+  .get(isAuthenticated, getWishList);
 
 module.exports = wishListRouter;
